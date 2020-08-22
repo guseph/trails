ca9a34b7cfb46f90aed31f4eeeb5710760ef030b
Kasey Chuang <chuangky@uci.edu>
2020-08-22 11:49:25 -0700

    created upload route and frontend upload boilerplate

--- trails/src/components/AddReceipt.js
+++ trails/src/components/AddReceipt.js
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import "./AddReceipt.css";
 
 const AddReceipt = () => {
+    const [selectedFile, setSelectedFile] = useState(null);
+    const [fileName, setFileName] = useState(null);
+
+    const onFileSelected = (e) => {
+        if (e.target.files[0]){
+            setSelectedFile(e.target.files[0]);
+            setFileName(e.target.files[0].name);
+        }
+    }
+
+    const onFileUpload = async (e) => {
+        // e.preventDefault();
+        // try{
+        //     if (selectedFile !== ''){
+        //         let fileData = new FormData();
+        //         // add timestamp to upload in case user uploads same image multiple times
+        //         fileData.set('receipt', selectedFile, `${Date.now()}-${selectedFile.name}`);
+        //         await axios({
+        //             method: 'post', 
+        //             url: "", // upload route URL
+        //             data: fileData,
+        //             headers: {'Content-Type': 'multipart/form-data'}
+        //         });
+        //     }
+           
+        // } catch (error){
+        //     console.log(`error: ${error}`)
+        // }
+       console.log("file upload yee!");
+        
+    }
     
     return (
-        <div>Add Receipt</div>
+        <div>
+            <h1>Add Receipt</h1>
+            <h3>Select Receipt</h3>
+            <form id = "file-form" className = "ui form" onSubmit = {(e) => onFileUpload(e)}>
+                <input className = "field" type = 'file' onChange={onFileSelected} />
+                <button id = "upload-btn" type = 'submit' className = "ui button">Upload Receipt</button>
+            </form>            
+
+        </div>
     )
 }
 
